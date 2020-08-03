9bcfcde17b3a7dcadad691396552cc82a755691e
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2020-08-03 10:05:14 +0100

    added morgan

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
